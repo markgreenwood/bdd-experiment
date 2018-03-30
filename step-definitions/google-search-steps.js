299246df2bd8b43ce11fe541f84ba7540f73c5da
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2018-03-30 14:07:39 -0700

    Finally everything working

--- step-definitions/google-search-steps.js
+++ step-definitions/google-search-steps.js
@@ -1,31 +1,33 @@
-/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-extraneous-dependencies, prefer-arrow-callback */
 const { expect } = require('chai');
-const { Given, Then, When } = require('cucumber');
+const { Given, Then, When, After } = require('cucumber');
 const webdriver = require('selenium-webdriver');
 
 const { By, until } = webdriver;
-const { helpers, page } = require('../features/page-objects/google-search');
 
 const browser = new webdriver.Builder()
   .forBrowser('chrome')
   .build();
 
+let result = '';
 
-Given(/^I have loaded Google$/, function () {
+Given(/^I have loaded Google$/, async function () {
   return browser.get('https://google.com');
 });
 
-When(/^I search Google for "([^"]*)"$/, function (string) {
-  return page.googleSearch.performSearch(string)
-    .then(() => {
-      console.log('Finding keywords', string);
-      return browser.wait(until.elementsLocated(By.css('div.g')), 10000);
-    })
-    .then(resp => console.log(resp[0].getText()));
+When(/^I search Google for "([^"]*)"$/, async function (string) {
+  await browser.findElement(By.name('q')).sendKeys(string, webdriver.Key.ENTER);
+  const searchResults = await browser.wait(until.elementsLocated(By.css('div.g')), 10000);
+  result = await searchResults[0].getText();
+  return result;
 });
 
 Then('The first result is {string}', function (string) {
-  return 'pending';
+  expect(result).to.contain(string);
+});
+
+After(async function () {
+  return browser.quit();
 });
 
 /* eslint-enable */
